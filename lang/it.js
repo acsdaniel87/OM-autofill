36eee24e99e0842ed8c04f3e791ebf5c29bbd50e
acsdaniel87 <acs.daniel.munka@gmail.com>
2025-07-23 15:01:20 +0200

    Create it.js

---
+++ lang/it.js
@@ -0,0 +1,13 @@
+window.OMLabels = {
+  invalidOmAlert: "ID OM non valido!",
+  confirmDelete: "Sei sicuro di voler eliminare questo OM?",
+  settingsTitle: "Impostazioni",
+  addLabel: "Aggiungi OM",
+  deleteLabel: "Elimina",
+  shareLink: "Copia link di condivisione",
+  openKirint: "Ricerca istituti KIRINT",
+  closePanel: "Chiudi",
+  inputPlaceholder: "es. 202797",
+  reloadAlert: "Ricaricamento della pagina...",
+  selectLanguageLabel: "Lingua dell’interfaccia"
+};
