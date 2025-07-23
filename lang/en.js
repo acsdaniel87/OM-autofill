a5dcb3ec1d9dd176b18fcba1c5080a6b4181a264
acsdaniel87 <acs.daniel.munka@gmail.com>
2025-07-23 14:56:55 +0200

    Update en.js

--- lang/en.js
+++ lang/en.js
@@ -1,10 +1,13 @@
 window.OMLabels = {
-  invalidOmAlert: "Invalid OM ID!",
+  invalidOmAlert: "Invalid OM identifier!",
   confirmDelete: "Are you sure you want to delete this OM?",
   settingsTitle: "Settings",
   addLabel: "Add OM",
   deleteLabel: "Delete",
   shareLink: "Copy share link",
-  openKirint: "KIRINT Intézménykereső",
-  closePanel: "Close"
+  openKirint: "KIRINT Institution Finder",
+  closePanel: "Close",
+  inputPlaceholder: "e.g. 202797",
+  reloadAlert: "Reloading the page...",
+  selectLanguageLabel: "Interface language"
 };
