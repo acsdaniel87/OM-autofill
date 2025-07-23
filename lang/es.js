5a877f14ba4641100bd3f6324b41c772b0c8b75c
acsdaniel87 <acs.daniel.munka@gmail.com>
2025-07-23 15:00:50 +0200

    Create es.js

---
+++ lang/es.js
@@ -0,0 +1,13 @@
+window.OMLabels = {
+  invalidOmAlert: "¡Identificador OM inválido!",
+  confirmDelete: "¿Estás seguro de que quieres eliminar este OM?",
+  settingsTitle: "Configuración",
+  addLabel: "Agregar OM",
+  deleteLabel: "Eliminar",
+  shareLink: "Copiar enlace para compartir",
+  openKirint: "Buscador de instituciones KIRINT",
+  closePanel: "Cerrar",
+  inputPlaceholder: "ej. 202797",
+  reloadAlert: "Recargando la página...",
+  selectLanguageLabel: "Idioma de la interfaz"
+};
