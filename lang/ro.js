0e58d544320493bc447f25a0e6357d6d1a57b6e3
acsdaniel87 <acs.daniel.munka@gmail.com>
2025-07-23 15:01:38 +0200

    Create ro.js

---
+++ lang/ro.js
@@ -0,0 +1,13 @@
+window.OMLabels = {
+  invalidOmAlert: "ID OM invalid!",
+  confirmDelete: "Sigur dorești să ștergi acest OM?",
+  settingsTitle: "Setări",
+  addLabel: "Adaugă OM",
+  deleteLabel: "Șterge",
+  shareLink: "Copiază linkul de partajare",
+  openKirint: "Căutare instituții KIRINT",
+  closePanel: "Închide",
+  inputPlaceholder: "ex. 202797",
+  reloadAlert: "Se reîncarcă pagina...",
+  selectLanguageLabel: "Limba interfeței"
+};
