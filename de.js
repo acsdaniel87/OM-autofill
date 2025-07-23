aa7787f23ce4bd170464041304ad23c44840c2e0
acsdaniel87 <acs.daniel.munka@gmail.com>
2025-07-23 13:33:55 +0200

    de.js

---
+++ de.js
@@ -0,0 +1,10 @@
+window.OMLabels = {
+  invalidOmAlert: "Ungültige OM-ID!",
+  confirmDelete: "Möchtest du diese OM-ID wirklich löschen?",
+  settingsTitle: "Einstellungen",
+  addLabel: "OM hinzufügen",
+  deleteLabel: "Löschen",
+  shareLink: "Teilen-Link kopieren",
+  openKirint: "KIRINT Intézménykereső",
+  closePanel: "Schließen"
+};
