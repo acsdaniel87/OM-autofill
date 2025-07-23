e63c41c2355d23cb4f3581a425877e0a0aaff2e8
acsdaniel87 <acs.daniel.munka@gmail.com>
2025-07-23 13:35:03 +0200

    fr.js

---
+++ fr.js
@@ -0,0 +1,10 @@
+window.OMLabels = {
+  invalidOmAlert: "Identifiant OM invalide !",
+  confirmDelete: "Voulez-vous vraiment supprimer cet identifiant OM ?",
+  settingsTitle: "Paramètres",
+  addLabel: "Ajouter un OM",
+  deleteLabel: "Supprimer",
+  shareLink: "Copier le lien de partage",
+  openKirint: "KIRINT Intézménykereső",
+  closePanel: "Fermer"
+};
